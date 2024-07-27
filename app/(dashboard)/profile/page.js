be61255d8cec417ebc17fa46ef892eda17ed2962
miha11-hash <d_mihaela@msn.com>
2024-07-27 04:40:47 +0300

    register users on mdb

--- app/(dashboard)/profile/page.js
+++ app/(dashboard)/profile/page.js
@@ -1,12 +1,29 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { Loader2, UserCheck2 } from "lucide-react";
-import { auth, currentUser } from "@clerk/nextjs/server";
+import { Loader2 } from "lucide-react";
+import { currentUser } from "@clerk/nextjs/server";
+import connectDB from "@/utils/database";
+import User from "@/models/userModel";
 
 async function ProfilePage() {
-  
   const user = await currentUser();
-console.log(user);
+  const { id, firstName, lastName, emailAddresses } = user;
+  const emailAddress = emailAddresses[0]?.emailAddress;
+
+  console.log(id, firstName, lastName, emailAddress);
+  console.log("User id is:", id);
+
+  //register users on mdb
+  await connectDB();
+  const existingUser = await User.findOne({ email: emailAddress });
+  if (!existingUser) {
+    await User.create({
+      firstName: firstName,
+      lastName: lastName,
+      email: emailAddress,
+      clerkId: id,
+    });
+  }
 
   return (
     <div className="flex align-center justify-center w-full h-full">
@@ -15,7 +32,6 @@ console.log(user);
           Testul cuprinde 30 de intrebari. Fiecare intrebare are un singur
           raspuns corect.{" "}
         </p>
-
         <p>
           Fiecare raspuns corect este notat cu 0,3 puncte. Se acorda un punct
           din oficiu.
@@ -23,7 +39,8 @@ console.log(user);
         <p>Timpul de lucru este de 2 ore.</p>
         <p className="text-center pt-6">Succes! &#128522;</p>
         <Button className="w-50 mt-6 mx-auto text-xl">
-          <Loader2 className="mr-2 animate-spin" /> <Link href="/profile/test">Incepe Testul</Link>
+          <Loader2 className="mr-2 animate-spin" />{" "}
+          <Link href="/profile/test">Incepe Testul</Link>
         </Button>
       </div>
     </div>
