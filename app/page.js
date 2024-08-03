69306dbc3e905976725e29f9558e85f81f75458d
miha11-hash <d_mihaela@msn.com>
2024-08-03 15:37:06 +0300

    Home page restyled

--- app/page.js
+++ app/page.js
@@ -1,30 +1,45 @@
-
 import { Button } from "../components/ui/button";
 import Link from "next/link";
 import { SignUpButton } from "@clerk/nextjs";
+import { Card, CardContent } from "../components/ui/card";
 
 function HomePage() {
   return (
-    <div className="h-screen w-screen flex justify-center items-center background-div">
-      <div
-        className="max-w-50 auto p-10 rounded"
-        style={{ backgroundColor: "rgb(192, 192, 192, 0.6)" }}
-      >
-        <h1 className="text-center text-2xl font-semibold pb-8 tracking-wide">
-          Test
-        </h1>
-        <h1 className="text-center text-2xl font-semibold pb-8 tracking-wide">
-          Management Financiar
-        </h1>
-        <Link
-          href="/profile"
-          className="w-50 ml-20 mt-6 relative p-2 rounded text-lg"
+    <div className="h-screen w-screen background-div">
+      <div className="card">
+        <Card
+          style={{ backgroundColor: "rgba(192, 192, 102, 0.4" }}
+          className="lg: p-10 sm: px-8"
         >
-          <Button className="text-lg">
-            <SignUpButton></SignUpButton>
-          </Button>
-        </Link>
+          <CardContent>
+            <h1
+              className="text-center text-4xl font-bold pb-12 tracking-wide text-white"
+              style={{ letterSpacing: "0.1rem" }}
+            >
+              Test
+            </h1>
+            <h1
+              className="text-center text-3xl font-bold pb-12 tracking-wide text-white"
+              style={{ letterSpacing: "0.1rem" }}
+            >
+              Financial Management
+            </h1>
+            <Link
+              href="/profile"
+              className="w-100 ml-20 mt-10 relative p-8 rounded text-lg"
+            >
+              <Button className="text-2xl px-10 py-6">
+                <SignUpButton></SignUpButton>
+              </Button>
+            </Link>
+          </CardContent>
+        </Card>
       </div>
+      <footer>
+        <h1 className="text-center text-white">
+          &copy; Mihaela Diaconu. All rights reserved.
+        </h1>
+      </footer>
     </div>
   );
 }
